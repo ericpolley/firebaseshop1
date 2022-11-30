6dd6eb0ba287681a922b5a82f5a01807f74a8b02
eric polley <ericpolley@live.com>
2022-11-30 11:53:56 -0800

    profile page name edit with firebase data change

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,20 @@
-import { getAuth } from 'firebase/auth'
+import { getAuth, updateProfile } from 'firebase/auth'
+import { doc, updateDoc } from 'firebase/firestore';
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import { db } from '../firebase';
 
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
-  const [formData] = useState({
+  const [changeDetail, setChangeDetail] = useState(false)
+  const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email
   })
   const {name, email} = formData;
+
 
   function onLogout() {
     auth.signOut();
@@ -17,6 +22,37 @@ export default function Profile() {
 
   }
 
+
+  function onChange(e) {
+
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+
+  }
+  async function onSubmit(){
+   try {
+    if(auth.currentUser.displayName !== name){
+      //update display name firebase
+      await updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+
+      //update name in firestore
+
+      const docRef = doc(db, "users", auth.currentUser.uid)
+      await updateDoc(docRef, {
+        name
+      });
+    }
+    toast.success('Profile details are updated!')
+    
+   } catch (error) {
+    toast.error("Could not update profile details....")
+    
+   }
+  }
   return (
     <>
     <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -27,14 +63,23 @@ export default function Profile() {
         <form >
           {/*Name Input*/}
 
-          <input type="text" id="name" value={name} disabled className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded-lg transition ease-in-out mb-4'/>
+          <input type="text" id="name" value={name} disabled={!changeDetail} onChange={onChange} 
+          className={`w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded-lg transition ease-in-out mb-4 
+          ${changeDetail && "bg-red-200 focus:bg-red-200"} `}/>
 
           {/* email input */}
 
-          <input type="email" id="email" value={email} disabled className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded-lg transition ease-in-out mb-4'/>
+          <input type="email" id="email" value={email} disabled 
+           className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded-lg transition ease-in-out mb-4'/>
           <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6'>
             <p className='flex items-center'>Do you want to change your name? 
-              <span className='text-red-600 hover:text-red-700 transition ease-in-out duration-240 ml-1 cursor-pointer '> Edit </span>
+              <span 
+              onClick={() => {
+                changeDetail && onSubmit()
+                setChangeDetail((prevState) => !prevState)
+               }}
+               className='text-red-600 hover:text-red-700 transition ease-in-out duration-240 ml-1 cursor-pointer' > 
+              {changeDetail ? "Apply change" : "Edit"} </span>
             </p>
             <p onClick={onLogout} 
             className='text-blue-600 hover:text-blue-800 transition ease-in-out duration-240 cursor-pointer'>Sign Out</p>
