30df2e854f33e034a35529f8a0835d687e680c95
eric polley <ericpolley@live.com>
2022-11-30 10:47:10 -0800

    create private-route profile custom hook. Add log out function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 import ForgotPassword from "./pages/ForgotPassword";
 import Home from "./pages/Home";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+
 
 function App() {
   return (
@@ -17,8 +19,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/offers" element={<Offers />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/profile" element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />} />
+        </Route>
+        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
 
